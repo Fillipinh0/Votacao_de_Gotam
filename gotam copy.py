fe24ac87ecd4b4c5f976ca8d23b0abbdf4c5592a
Fillipinh0 <128330975+Fillipinh0@users.noreply.github.com>
2024-06-30 22:42:22 -0300

    O professor pediu para explicar os codigos então adcionei alguns comentarios

--- gotam copy.py
+++ gotam copy.py
@@ -1,10 +1,11 @@
 #Professor passou esse trabalho com o tema e falou que 20 pessoas iam votar
-from verificar import verifica_ganhador 
+from verificar import verifica_ganhador # importei a função
 def votacao():
-    candidatos = [0, 0, 0, 0]  # Vetor 0 = Coringa, 1 = Mulher Gato, 2 = Charada, 3 = Hera Venenosa
-
+    candidatos = [0, 0, 0, 0]  # Indice 0 = Coringa, 1 = Mulher Gato, 2 = Charada, 3 = Hera Venenosa
+    #Faz um loop para o programa rodar 20 vezes
     for i in range(20):
         quem = input("-Digite 1 para votar no Coringa \n-Digite 2 para votar na Mulher Gato \n-Digite 3 para votar Charada\n-Digite 4 Para votar na Hera venenosa \n-se número >= 5 o voto é nulo \n-Vote:  ")
+        #Optei pelo match para verificar em qual candidato a pessoa votou e adcionar um voto ao array
         match quem:
             case "1":
                 candidatos[0] += 1
@@ -15,6 +16,6 @@ def votacao():
             case "4":
                 candidatos[3] += 1
     
-    verifica_ganhador(candidatos)
+    verifica_ganhador(candidatos)#função que esta na outra pagina
 
 votacao()
--- verificar.py
+++ verificar.py
@@ -1,6 +1,7 @@
 # Vetor 0 = coringa, 1 = mulher gato, 2 = charada e 3 = hera venenosa  <--- So para lembrar
-# Verifica qual candidato tem mais votos e imprime o resultado
+# Verifica qual candidato tem mais votos e imprime o resultado. 
 def verifica_ganhador(candidatos):
+    #Usei a porta 'and' e os if/elif para analisar cada candito e ver qual tinha o maior numero de votos 
     if candidatos[0] > candidatos[1] and candidatos[0] > candidatos[2] and candidatos[0] > candidatos[3]:
         print("O ganhador é o Coringa")
     elif candidatos[1] > candidatos[0] and candidatos[1] > candidatos[2] and candidatos[1] > candidatos[3]:
@@ -10,4 +11,5 @@ def verifica_ganhador(candidatos):
     elif candidatos[3] > candidatos[0] and candidatos[3] > candidatos[1] and candidatos[3] > candidatos[2]:
         print("A ganhadora é a Hera Venenosa")
     else:
-        print("Todos votaram nulo! Gotham vai ser destruída pelas bombas")
+        #se der empate ou todos votarem nulo
+        print("Gotem vai ser explodida pelas bombas!")
